2a623704e397d89cb49f47bc2299922e7f7a523e
BRIGHAM450 <brighamcarreraventura@gmail.com>
2025-09-06 21:05:47 -0500

    C# Prep 02

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,79 @@
 using System;
+using System.Drawing;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percent?: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput); // Convierte el string a int
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // adding + or -
+        string sign = "";
+
+        int lastDigit = grade % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // Handle the A+ grades
+        if (grade >= 93)
+        {
+            sign = "";
+        }
+
+        // Handle the F+ and F- grades
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Stay focused and you'll get it next time!");
+        }
+
     }
 }
