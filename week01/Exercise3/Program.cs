ba7d9f860d70621a914333a3e3a920f79920f5a3
BRIGHAM450 <brighamcarreraventura@gmail.com>
2025-09-06 23:58:38 -0500

    C# Prep 03

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        
+        // Main loop to play multiple times
+        while (playAgain.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101); // 1-100
+            int guess = -1;
+            int guessCount = 0; // Guess counter
+
+            Console.WriteLine("Welcome to the Number Guessing Game!");
+            Console.WriteLine("I've chosen a number between 1 and 100.");
+
+            // Guessing loop
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++; // Increment counter
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            // Show number of attempts
+            Console.WriteLine($"Congratulations! You guessed it in {guessCount} attempts.");
+
+            // Ask to play again
+            Console.Write("Would you like to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+            Console.WriteLine(); // Blank line to separate games
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
